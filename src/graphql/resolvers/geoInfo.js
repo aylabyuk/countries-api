10785ad2c5d847b94ca7eab0fda6b690cde10919
Oriel Absin <Oriel Vinci Absin>
2019-03-23 14:10:29 +0800

    add single query for a country

--- src/graphql/resolvers/geoInfo.js
+++ src/graphql/resolvers/geoInfo.js
@@ -7,6 +7,14 @@ export default {
     continents: async (_, args, { utils }) => {
       const res = await utils.geonamesUtils.getContinents();
       return res;
+    },
+    country: async (_, { continentName, countryName }, { utils }) => {
+      const res = await utils.geonamesUtils
+        .getCountriesByContinentName(continentName)
+        .then(results => {
+          return results.filter(country => country.countryName === countryName)[0];
+        });
+      return res;
     }
   }
 };
