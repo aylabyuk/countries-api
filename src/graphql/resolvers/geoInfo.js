678a99a42e50bdcc05585cc963296f9633403da9
Oriel Absin <Oriel Vinci Absin>
2019-04-18 15:27:24 +0800

    process currency and language codes

--- src/graphql/resolvers/geoInfo.js
+++ src/graphql/resolvers/geoInfo.js
@@ -1,4 +1,25 @@
+import getSymbolFromCurrency from 'currency-symbol-map';
+import isoConv from 'iso-language-converter/index.es5';
+
 export default {
+  Country: {
+    currency(country) {
+      return {
+        code: country.currencyCode,
+        symbol: getSymbolFromCurrency(country.currencyCode)
+      };
+    },
+    languages(country) {
+      const langArr = country.languages.split(',').map(l => {
+        return {
+          code: l,
+          name: isoConv(l.split('-')[0])
+        };
+      });
+
+      return langArr;
+    }
+  },
   Query: {
     countries: async (_, { continentName }, { utils }) => {
       const res = await utils.geonamesUtils.getCountriesByContinentName(continentName);
@@ -15,6 +36,12 @@ export default {
           return results.filter(country => country.countryName === countryName)[0];
         });
       return res;
+    },
+    continent: async (_, { continentName }, { utils }) => {
+      const res = await utils.geonamesUtils.getContinents().then(cont => {
+        return cont.filter(item => item.name === continentName);
+      });
+      return res[0];
     }
   }
 };
